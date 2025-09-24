794509e3bc56b9af03e06a2a15f5ccbe120c1398
Nicholas Von Huben <nicholas.vonhuben@gmail.com>
2025-09-24 21:59:10 +1000

    WIP: Config improvements

--- yakka/yakka_config.cpp
+++ yakka/yakka_config.cpp
@@ -2,13 +2,17 @@
 #include "yakka_config.hpp"
 #include "yakka_component.hpp"
 #include "yakka_workspace.hpp"
+#include "yakka_project.hpp"
 #include "spdlog.h"
+#include <ryml.hpp>
+#include <ryml_std.hpp> // For std::string and std::vector interop
 #include <httplib.h>
 #include <chrono>
 
 namespace yakka {
 
-void start_config_server(yakka::workspace& workspace, bool& server_running) {
+void start_config_server(yakka::workspace &workspace, bool &server_running)
+{
   httplib::Server server;
 
   if (!server.is_valid()) {
@@ -16,31 +20,100 @@ void start_config_server(yakka::workspace& workspace, bool& server_running) {
     return;
   }
 
-  server.Options("/(.*)", [&](const httplib::Request& /*req*/, httplib::Response& res) {
+  // Find components in the workspace that provide server data and mount points
+
+  server.Options("/(.*)", [&](const httplib::Request & /*req*/, httplib::Response &res) {
     res.set_header("Access-Control-Allow-Methods", "*");
     res.set_header("Access-Control-Allow-Headers", "*");
     res.set_header("Access-Control-Allow-Origin", "*");
     res.set_header("Connection", "close");
   });
 
-//   workspace.init(".");
+  server.set_pre_routing_handler([](const auto &req, auto &res) {
+    if (req.path.starts_with("/api/") || req.path.starts_with("/assets/")) {
+      return httplib::Server::HandlerResponse::Unhandled;
+    }
+    std::ifstream file("./components/configurator/index.html");
+    std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+    res.set_content(content, "text/html");
+    file.close();
+    return httplib::Server::HandlerResponse::Handled;
+  });
+  server.set_mount_point("/assets", "./components/configurator/assets");
 
-  server.Get("/api/components", [&](const httplib::Request& req, httplib::Response& res) {
+  server.Get("/api/components", [&](const httplib::Request &req, httplib::Response &res) {
     spdlog::info("GET /api/components\n");
     res.set_header("Access-Control-Allow-Origin", "*");
     res.set_content(workspace.local_database.dump(), "application/json");
   });
 
-  server.Get("/api/projects", [&](const httplib::Request& req, httplib::Response& res) {
+  server.Get("/api/projects", [&](const httplib::Request &req, httplib::Response &res) {
     spdlog::info("GET /api/projects\n");
     res.set_header("Access-Control-Allow-Origin", "*");
     res.set_content(workspace.projects.dump(), "application/json");
   });
 
-  server.Get("/api/component/:id", [&](const httplib::Request& req, httplib::Response& res) {
+  server.Get("/api/project/:id", [&](const httplib::Request &req, httplib::Response &res) {
+    spdlog::info("GET /api/project/{}\n", req.path_params.at("id"));
+    res.set_header("Access-Control-Allow-Origin", "*");
+    auto project = req.path_params.at("id");
+    if (!workspace.projects.contains(project)) {
+      res.status = 404;
+      return;
+    }
+    auto project_summary_file = std::filesystem::path{ workspace.projects[project]["path"].get<std::string>() } / project_summary_filename;
+    std::ifstream file(project_summary_file, std::ios::binary);
+    if (file) {
+      // Read the entire file content into a string
+      std::string file_content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+      res.set_content(file_content, "application/json"); // Set appropriate Content-Type
+    } else {
+      res.status = 404;
+      return;
+    }
+  });
+
+  server.Post("/api/project/:id/data", [&](const httplib::Request &req, httplib::Response &res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+    spdlog::get("console")->info("POST /api/project/{}/data\n", req.path_params.at("id"));
+    auto project_id = req.path_params.at("id");
+
+    // Load up project summary and see if the project file exists
+    yakka::project the_project(project_id, workspace);
+    the_project.init_project();
+
+    // If not, create a new project file
+    if (the_project.project_file.empty() || !fs::exists(the_project.project_file)) {
+      the_project.create_project_file();
+    }
+
+    // Now parse the incoming JSON data and update the project file
+    try {
+      ryml::Tree incoming_data = ryml::parse_in_arena(ryml::to_csubstr(req.body));
+
+      std::fstream project_file_stream(the_project.project_file, std::ios::in | std::ios::out);
+      std::stringstream buffer;
+      buffer << project_file_stream.rdbuf();
+      std::string yaml_str = buffer.str();
+      ryml::Tree project_data = ryml::parse_in_arena(ryml::to_csubstr(yaml_str));
+
+      project_data.merge_with(&incoming_data);
+
+      project_file_stream.seekp(0);
+      project_file_stream << project_data;
+      project_file_stream.close();
+      res.set_content("{\"status\": \"OK\"}", "application/json");
+    } catch (const nlohmann::json::parse_error &e) {
+      spdlog::error("Failed to parse incoming JSON data: {}\n", e.what());
+      res.status = 400;
+      return;
+    }
+  });
+
+  server.Get("/api/component/:id", [&](const httplib::Request &req, httplib::Response &res) {
     spdlog::info("GET /api/component/{}\n", req.path_params.at("id"));
     res.set_header("Access-Control-Allow-Origin", "*");
-    auto component = req.path_params.at("id");
+    auto component      = req.path_params.at("id");
     auto component_path = workspace.local_database.get_component(component);
     if (component_path.has_value()) {
       yakka::component component_data;
@@ -51,14 +124,14 @@ void start_config_server(yakka::workspace& workspace, bool& server_running) {
     }
   });
 
-  server.set_error_handler([](const httplib::Request& /*req*/, httplib::Response& res) {
-    const char* fmt = "<p>Error Status: <span style='color:red;'>%d</span></p>";
+  server.set_error_handler([](const httplib::Request & /*req*/, httplib::Response &res) {
+    const char *fmt = "<p>Error Status: <span style='color:red;'>%d</span></p>";
     char buf[BUFSIZ];
     snprintf(buf, sizeof(buf), fmt, res.status);
     res.set_content(buf, "text/html");
   });
 
-  server.set_logger([](const httplib::Request& req, const httplib::Response& res) {
+  server.set_logger([](const httplib::Request &req, const httplib::Response &res) {
     spdlog::debug(dump_request_response(req, res));
   });
 
@@ -68,12 +141,13 @@ void start_config_server(yakka::workspace& workspace, bool& server_running) {
   server_running = false;
 }
 
-std::string dump_headers(const httplib::Headers& headers) {
+std::string dump_headers(const httplib::Headers &headers)
+{
   std::string s;
   char buf[BUFSIZ];
 
   for (auto it = headers.begin(); it != headers.end(); ++it) {
-    const auto& x = *it;
+    const auto &x = *it;
     snprintf(buf, sizeof(buf), "%s: %s\n", x.first.c_str(), x.second.c_str());
     s += buf;
   }
@@ -81,7 +155,8 @@ std::string dump_headers(const httplib::Headers& headers) {
   return s;
 }
 
-std::string dump_request_response(const httplib::Request& req, const httplib::Response& res) {
+std::string dump_request_response(const httplib::Request &req, const httplib::Response &res)
+{
   std::string s;
   char buf[BUFSIZ];
 
@@ -92,7 +167,7 @@ std::string dump_request_response(const httplib::Request& req, const httplib::Re
 
   std::string query;
   for (auto it = req.params.begin(); it != req.params.end(); ++it) {
-    const auto& x = *it;
+    const auto &x = *it;
     snprintf(buf, sizeof(buf), "%c%s=%s", (it == req.params.begin()) ? '?' : '&', x.first.c_str(), x.second.c_str());
     query += buf;
   }
